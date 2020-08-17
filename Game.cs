ce3fd68dd713a4f38439eade581910c4d4622c89
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-08-17 11:52:45 -0700

    Created method object to choose type of game play.

--- Game.cs
+++ Game.cs
@@ -21,6 +21,8 @@ namespace RPSLS
         {
             pointsNeededToWin = 3;
             playerOne = new Human();
+            gestures = new List<string>() { "rock", "paper", "scissors", "lizard", "spock" };
+            actions = new List<string>() { "crushes", "cuts", "covers", "poisons", "smashes", "decapitates", "eats", "disproves", "vaporizes" };
         }
 
         //Member Methods (Can do)
@@ -34,7 +36,46 @@ namespace RPSLS
         }
         public void ChooseGameMode()
         {
+            Console.WriteLine("Which type of game would you like play?");
+            Console.WriteLine("1: Human vs. Human");
+            Console.WriteLine("2: Human vs. Conputer");
+            gameType = Console.ReadLine();
+            return gameType;
+        }
+        public void ChooseTypeOfPlayerTwo()
+        {
+            //switch-case object
+            //instantiates playerTwo as Human or Computer
+            //with userInput
+            bool gameSelected = false;
+            
+            while (gameSelected == false)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        playerOne = new Human();
+                        playerOne.SetName();
+                        playerTwo = new Human();
+                        playerTwo.SetName();
+                        gameSelected = true;
+                        break;
 
+                    case "2":
+                        playerOne = new Human();
+                        playerOne.SetName();
+                        playerTwo = new Computer;
+                        playerTwo.SetName();
+                        gameSelected = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid selection, please choose 1 or 2.");
+                        userInput = Console.ReadLine();
+                        break;
+
+                }
+            } 
         }
     }
 }
